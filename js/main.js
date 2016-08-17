02e09910770b2184de09155c2de3b74edf532dd9
Yauhen Hursky <e.gursky@mobiles.by>
2016-08-17 10:41:38 +0300

    one page app

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
 var mainPage = $(".main-page-content");
+var statisticPage = $(".static-page-content");
 var menus = $('#menu-hrefs li a');
-a = menus;
 /*switch	()*/
 var url = window.location.hash;
 var hash = url.substring(url.indexOf('#')+1); // '#foo'
 showContent(hash);
 
+menus.each(function(i){
+	if (hash == menus.eq(i).attr('data-content')) {
+		menus.attr('class', '');
+		menus.eq(i).attr('class', 'current');
+	}
+})
 menus.on('click', function(e){
 	menus.attr('class', '');
-	$(e.target).attr('class', 'current')
+	$(e.target).attr('class', 'current');
 	showContent($(e.target).attr('data-content'));
 });
 
 function showContent(content){
-	console.log(content)
 	switch(content){
 		case 'main-page-content':
 		mainPage.css('display', 'block');
-		$('body').css('background-image', '')
+		statisticPage.css('display', 'none');
+		$('body').css('background-image', '');
 		break;
 
 		case 'statistics':
 		mainPage.css('display', 'none');
-		$('body').css('background-image', 'url(images/head3.png)')
+		statisticPage.css('display', 'block');
+		$('body').css('background-image', 'url(images/head3.png)');
 		break;
+
+		default:
+		mainPage.css('display', 'block');
+		statisticPage.css('display', 'none');
+		$('body').css('background-image', '');
+		break;
+
 	}
 }
